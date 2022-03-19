a25b7108badafbfdb699072b78c7f277bffa380d
Zach <shaw.zacht@gmail.com>
2022-03-19 15:06:56 -0700

    commit tree cb3343b54839484ef8189dc0ad5bb38f215f1355

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
-}
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+  static titleize(string) {
+    let splitStrings = string.split(" ");
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'is']
+    let sentence = splitStrings.map(word => {
+      
+      let formatted = exceptions.indexOf(word) === -1 ?
+      word.charAt(0).toUpperCase() + word.slice(1) : word;
+    
+      return formatted;
+    });
+    let final = sentence.join(" ")
+    
+    return this.capitalize(final)
+  }
+} 
+  
+Formatter.titleize("A Tale of Two Cities")
+Formatter.titleize("Getting Giggles")
+
